7e9e78102295e991799a98ff2490628aecdca943
Fred Blanc <findel@free.fr>
2020-12-15 11:11:34 +0100

    Fix laravel namespace with vendor set in preferences

--- src/namespace.js
+++ src/namespace.js
@@ -4,70 +4,106 @@ const fs = require('fs')
 const config = require('./config')
 
 function getNamespaceFromPath(filename) {
-    const workspaceFolder = vscode.workspace.getWorkspaceFolder(vscode.Uri.file(filename)).uri.fsPath
-    const composerFile = workspaceFolder + path.sep + config.getConfig('composerJson')
+    let vendorData = getNamespaceFromComposer(filename)
+
     let relativeFilename = vscode.workspace.asRelativePath(filename)
-
-    let content = ''
-    try {
-        content = fs.readFileSync(composerFile)
-    } catch {
-        content = '{}'
+    if (vendorData.startsWith.length > 0) {
+        relativeFilename = relativeFilename.substr(vendorData.startsWith.length)
     }
-    const composer = JSON.parse(content)
 
-    let nsVendor = config.getConfig('vendor')
+    const pathElements = relativeFilename.split(path.sep)
+    if (pathElements.length < 2) {
+        return vendorData.namespace
+    }
+
+    let namespace = pathElements.slice(0, pathElements.length - 1).map(element => {
+        return element.charAt(0).toUpperCase() + element.slice(1)
+    }).join('\\')
+
+    if (vendorData.namespace === '') {
+        return namespace
+    }
+
+    if (namespace === '') {
+        return vendorData.namespace
+    }
+
+    return vendorData.namespace + '\\' + namespace
+}
+
+function getComposerFileFor(filename)
+{
+    const configComposer = config.getConfig('composerJson')
+    if (configComposer.length < 1) {
+        return null
+    }
+
+    const workspaceFolder = vscode.workspace.getWorkspaceFolder(vscode.Uri.file(filename)).uri.fsPath
+    return workspaceFolder + path.sep + configComposer
+}
+
+function getNamespaceFromComposer(filename)
+{
+    let defaultVendor = {
+        'namespace': config.getConfig('vendor'),
+        'startsWith': ''
+    }
+
+    const composer = loadJson(getComposerFileFor(filename))
+    if (! composer) {
+        return defaultVendor
+    }
+
+    const relativeFilename = vscode.workspace.asRelativePath(filename)
+        .replace(path.sep, '/')
 
     const sources = ['autoload', 'autoload-dev']
-    for (const s in sources) {
-        const env = sources[s]
+    for (let index = 0; index < sources.length; index++) {
+        const env = sources[index]
 
         if (composer[env] && composer[env]['psr-4']) {
-            let folder = composer[env]['psr-4']
+            for (let vendor in composer[env]['psr-4']) {
+                const folder = composer[env]['psr-4'][vendor]
 
-            for (const vendor in folder) {
-                if (relativeFilename.startsWith(folder[vendor])) {
-                    nsVendor = vendor
-                    if (nsVendor.endsWith('\\')) {
-                        nsVendor = nsVendor.substr(0, nsVendor.length -1)
+                if (relativeFilename.startsWith(folder)) {
+                    if (vendor.endsWith('\\')) {
+                        vendor = vendor.substr(0, vendor.length -1)
                     }
-                    relativeFilename = relativeFilename.substr(folder[vendor].length)
+
+                    return {
+                        'namespace': vendor,
+                        'startsWith': folder
+                    }
                 }
             }
         }
     }
 
-    const pathElements = relativeFilename.split(path.sep)
-
-    if (pathElements.length < 2) {
-        return nsVendor
+    if (relativeFilename.startsWith('src/')) {
+        defaultVendor.startsWith = 'src/'
+    } else if (relativeFilename.startsWith('tests/')) {
+        defaultVendor.startsWith = 'tests/'
+    } else if (defaultVendor.namespace !== '' && relativeFilename.startsWith('app/')) {
+        defaultVendor.startsWith = 'app/'
     }
 
-    let startIndex = 0
+    return defaultVendor
+}
 
-    if (pathElements[0] === 'src' || pathElements[0] === 'tests') {
-        startIndex = 1;
-    } else if (nsVendor === '' && pathElements[0] === 'app') {
-        nsVendor = 'App';
-        startIndex = 1;
+function loadJson(filename) {
+    let content = '{}'
+    try {
+        content = fs.readFileSync(filename)
+    } catch {
+        return null
     }
 
-    let namespace = pathElements.slice(
-        startIndex,
-        pathElements.length - 1
-    ).map(pathElement => {
-        return pathElement.charAt(0).toUpperCase() + pathElement.slice(1)
-    }).join('\\')
-
-    if (nsVendor.length > 0) {
-        if (namespace.length > 0) {
-            return nsVendor + '\\' + namespace
-        } else {
-            return nsVendor
-        }
+    const json = JSON.parse(content)
+    if (! json) {
+        return null
     }
 
-    return namespace
+    return json
 }
 
 exports.getNamespaceFromPath = getNamespaceFromPath
