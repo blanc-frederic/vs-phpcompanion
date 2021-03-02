289eb3f7f1b8ef9f4df28826c81ffdb875c062e9
Fred Blanc <findel@free.fr>
2021-03-02 22:30:58 +0100

    Fix No stats message when only 1 test

--- src/PHPUnitParser.js
+++ src/PHPUnitParser.js
@@ -5,7 +5,7 @@ class PHPUnitParser {
             .split('\n')
             .map(
                 value => value.match(/Tests: (?<tests>\d+)[^,\.]*(, Assertions: (?<assertions>\d+)[^,\.]*)?(, Failures: (?<failures>\d+)[^,\.]*)?(, Errors: (?<errors>\d+)[^,\.]*)?(, Skipped: (?<skipped>\d+)[^,\.]*)?/)
-                    || value.match(/(OK )?\((?<tests>\d+) tests, (?<assertions>\d+) assertions\)/)
+                    || value.match(/(OK )?\((?<tests>\d+) tests?, (?<assertions>\d+) assertions?\)/)
             )
             .filter(value => value !== null)
             .pop()
